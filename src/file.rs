6da5f687283e851f1a6ab0ed2081069eb4b6dcc0
朱海峰 <zhuhaifeng@parkingwang.com>
2019-06-17 18:46:23 +0800

    init repo

--- src/file.rs
+++ src/file.rs
@@ -35,7 +35,6 @@ impl<'a> FileWatcher<'a> {
 
     // 递归遍历子目录下的所有目录 这里其实不需要返回值太丑了
     fn read_dir(&mut self, p: &Path) -> io::Result<()> {
-        //last
         if p.is_dir() {
             for entry in fs::read_dir(p)? {
                 let file_or_path = entry?.path();
@@ -43,9 +42,10 @@ impl<'a> FileWatcher<'a> {
                     // 获取当前file的metadata
                     let fmeta = fs::metadata(file_or_path).unwrap(); 
                     // 如果file一个月没有改动过了那就删除
-                    // if fmeta.modified()  {
+                    let mut last_modified_time = fmeta.modified();
+                     if std::time::Instant::now() > (last_modified_time + std::time::Duration::from_secs(60 *60 *24 *30))  {
                         
-                    // }
+                     }
                     // 可能需要chunk
                     println!("{:?}", fmeta.modified());
                     
