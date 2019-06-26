2c45d388a54b87ca341d50ab1d9ab25b3b9c5ea1
朱海峰 <zhuhaifeng@zhuhaifengdeMacBook-Pro.local>
2019-06-26 17:23:40 +0800

    file

--- src/file.rs
+++ src/file.rs
@@ -1,7 +1,5 @@
 use std::ops::Add;
 use std::path::Path;
-use std::thread::sleep;
-use std::time::Duration;
 use std::{
     fs,
     fs::File,
@@ -10,8 +8,9 @@ use std::{
 };
 
 const WATCH_TICKER: u64 = 6;
-const CHUNK_FILE_SIZE: u64 = (2 << 20) * 2;
+const CHUNK_FILE_SIZE: u64 = 1024 * 1024 * 2; // 2mb
 const MONTHLY: u64 = 60 * 60 * 24 * 30;
+pub const PATH: &str = "/Users/zhuhaifeng/Desktop/workspace/rustcode/simple-file-watcher/file/";
 pub struct FileWatcher<'a> {
     // 指定多少时间刷新获取一次目录. 默认10分钟
     pub watcher_ticker: u64,
@@ -35,20 +34,77 @@ impl<'a> FileWatcher<'a> {
     fn traverse_dir(&mut self) -> &Self {
         // @todo exclude path logic
         for p in self.path.clone() {
-            let _ = self.read_dir(&p);
+            match self.chunk(&p) {
+                Ok(_) => println!("nothing todo"),
+                Err(err) => println!("err: {:?}", err.to_string()),
+            }
         }
         self
     }
 
-    fn read_dir(&mut self, p: &Path) -> io::Result<()> {
+    fn chunk(&mut self, p: &Path) -> io::Result<()> {
         if p.is_dir() {
             for entry in fs::read_dir(p).unwrap() {
                 let file_or_path = entry.unwrap().path();
                 if file_or_path.is_dir() {
-                    self.read_dir(&file_or_path).unwrap();
-                } else {}
+                    self.chunk(&file_or_path).unwrap()
+                } else {
+                    println!("需要切割的文件: {:?}", file_or_path);
+                    let mut fd = File::open(&file_or_path).unwrap();
+                    let origin_file_name = file_or_path.file_name().unwrap().to_str().unwrap();
+                    let origin_file_size = file_or_path.metadata().unwrap().len();
+                    println!("文件大小: {:?}", origin_file_size);
+                    let remaining_size = origin_file_size % CHUNK_FILE_SIZE;
+                    println!("不能被整除, 剩余量: {:?}bytes", remaining_size);
+                    let suffix = 0;
+                    // 先处理剩余的
+                    if remaining_size > 0 {
+                        let new_file_name = self.new_file_name(origin_file_name, suffix);
+                        println!("新的文件名: {:?}", new_file_name);
+                        // 从源文件头部读取remaining_size大小的buf
+                        let mut remaining_buf = Box::new(vec![0; remaining_size as usize]);
+                        match fd.read(&mut remaining_buf) {
+                            Ok(_) => {
+                                let mut new_file = File::create(new_file_name).unwrap();
+                                new_file.write(&mut remaining_buf).unwrap();
+                                println!("处理剩余buf成功! {:?}", remaining_buf.len());
+                                // 处理剩余的文件
+                                let chunk_count = origin_file_size / CHUNK_FILE_SIZE;
+                                println!("chunk_count! {:?}", chunk_count);
+                                let mut seek_flag = remaining_size;
+                                for index in 1..(chunk_count + 1) {
+                                    fd.seek(SeekFrom::Start(seek_flag)).unwrap();
+                                    let mut buf = Box::new(vec![0; CHUNK_FILE_SIZE as usize]);
+                                    match fd.read(&mut buf) {
+                                        Ok(_) => {
+                                            let new_file_name = self.new_file_name(origin_file_name, index as i32);
+                                            println!("文件名! {:?}", new_file_name);
+                                            let mut new_file = File::create(new_file_name)?;
+                                            new_file.write(&mut buf).unwrap();
+                                            println!("处理剩余buf成功! {:?}", buf.len());
+                                            // 增加偏移量
+                                            seek_flag += CHUNK_FILE_SIZE;
+                                        }
+                                        Err(err) => println!("err: {:?}", err.to_string()),
+                                    }
+                                }
+                            }
+                            Err(err) => println!("err: {:?}", err.to_string()),
+                        }
+                    }
+                }
             }
+        } else {
+            println!("p: {:?} ", p);
         }
         Ok(())
+    }
+
+    fn new_file_name(&mut self, origin_file_name: &str, suffix: i32) -> String {
+        PATH.to_owned() + origin_file_name + "_" + suffix.to_string().as_str()
+    }
+
+    pub fn run(&mut self) {
+        self.traverse_dir();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::path::Path;
 pub mod file;
-
 fn main() {
     let mut path = vec![Box::new(Path::new(
-        "/Users/zhuhaifeng/Desktop/workspace/rust/filewatcher/file",
+        "/Users/zhuhaifeng/Desktop/workspace/rustcode/simple-file-watcher/file",
     ))];
     let mut exclude_path = vec![Box::new(Path::new(
-        "/Users/zhuhaifeng/Desktop/workspace/rust/filewatcher/target",
+        "/Users/zhuhaifeng/Desktop/workspace/rustcode/simple-file-watcher",
     ))];
     let mut file_watcher: file::FileWatcher = file::new(&mut path, &mut exclude_path);
     file_watcher.run()
